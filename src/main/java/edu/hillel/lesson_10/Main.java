fb71d8da9d961add9877a28feecdbee24e5ec7d5
mykola-tsyb <mykola.tsyb@oracle.com>
2023-09-11 13:27:44 +0200

    Add lesson 10, 11

---
+++ src/main/java/edu/hillel/lesson_10/Main.java
@@ -0,0 +1,11 @@
+package edu.hillel.lesson_10;
+
+import org.example.utils.CommonUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CommonUtils commonUtils = new CommonUtils();
+        System.out.println(commonUtils.utilMethod("sdfdsfdsf"));
+    }
+}
---
+++ src/main/java/edu/hillel/lesson_11/Main.java
@@ -0,0 +1,34 @@
+package edu.hillel.lesson_11;
+
+import com.google.common.collect.ImmutableList;
+
+public class Main {
+
+    private String str1;
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        for (String arg : args) {
+
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Math.addExact(i, i);
+            System.out.println("sdfsfdsfsdfsdf");
+            System.out.println("new string");
+        }
+
+        ImmutableList.of("1", "2", "3");
+        System.out.println("dfgfdgdfgfdg");
+    }
+}
