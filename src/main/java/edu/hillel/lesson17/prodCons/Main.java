dbca538c293eb696c9728068c783786b1d5f22e0
mykola-tsyb <mykola.tsyb@oracle.com>
2023-10-06 12:49:46 +0200

    Edit lesson 17

--- src/main/java/edu/hillel/lesson17/prodCons/Main.java
+++ src/main/java/edu/hillel/lesson17/prodCons/Main.java
@@ -4,37 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
-
-
-        List<Thread> producers = new ArrayList<>();
-
-
-        for (int i = 0; i < 10; i++) {
-            producers.add(new Thread(() -> IntStream.range(0, 10).forEach(n -> queue.add(randomValue()))));
-        }
-
-
-        List<Thread> consumers = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            consumers.add(new Thread(() -> IntStream.range(0,10).forEach(n-> System.out.println(queue.poll()))));
-        }
-
-
-        for (Thread producer : producers) {
-            producer.start();
-        }
-
-        for (Thread consumer : consumers) {
-            consumer.start();
-        }
-
+        List<Thread> producers;
+        List<Thread> consumers;
+        producers = IntStream.range(0, 10)
+                .mapToObj(i -> new Thread(() -> IntStream.range(0, 10).forEach(n -> queue.add(randomValue()))))
+                .collect(Collectors.toList());
+        consumers = IntStream.range(0, 5)
+                .mapToObj(i -> new Thread(() -> IntStream.range(0, 10).forEach(n -> System.out.println(queue.poll()))))
+                .collect(Collectors.toList());
+        producers.forEach(Thread::start);
+        consumers.forEach(Thread::start);
     }
 
     private static Integer randomValue() {
