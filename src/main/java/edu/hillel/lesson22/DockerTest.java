3cec38bd4940fd69576554905f651a9083b2fcd1
mykola-tsyb <mykola.tsyb@oracle.com>
2023-10-23 20:29:30 +0200

    Create lesson 22

---
+++ src/main/java/edu/hillel/lesson22/DockerTest.java
@@ -0,0 +1,11 @@
+package edu.hillel.lesson22;
+
+public class DockerTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println(" Container was run!!!!!");
+            Thread.sleep(5000);
+        }
+    }
+}
